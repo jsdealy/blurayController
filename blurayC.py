5d1e5b59b7423df28e2b28c909e5fd80ce363092
jsdealy <dealyjustins@gmail.com>
2024-06-26 22:43:36 -0400

    first

---
+++ blurayC.py
@@ -0,0 +1,38 @@
+import broadlink
+import pickle
+from sys import argv
+
+if len(argv) == 0:
+    print("No command given.")
+    exit(1)
+
+deviceIP = "192.168.1.139"
+if len(argv) >= 2 and argv[1].startswith("192.168"):
+    deviceIP = argv[2]
+
+count = 0
+while True:
+    try:
+        if count > 5:
+            print("Failed to connect to device & authorize.")
+            exit(1)
+        device = broadlink.hello(deviceIP)
+        device.auth()
+        break
+    except:
+        count += 1
+
+packets = {}
+
+try:
+    with open("/home/justin/.broadlinkBlurayControllerPackets", "rb") as file:
+        packets = pickle.load(file)
+except:
+    print("Failed to open packet dictionary from file.")
+    exit(1)
+
+try:
+    device.send_data(packets[argv[1]])
+except:
+    print("Failed to send data.")
+    exit(1)
---
+++ blurayCSetup.py
@@ -0,0 +1,52 @@
+import broadlink
+import pickle
+
+deviceIP = "192.168.1.139"
+while True:
+    try:
+        device = broadlink.hello(deviceIP)
+        print(f"Device found: {device}")
+        device.auth()
+        break
+    except:
+        deviceIP = input("What's the broadlink local IP: ")
+
+packets = {
+    "menu": b'x00',
+    "back": b'x00',
+    "okay": b'x00',
+    "up": b'x00',
+    "down": b'x00',
+    "left": b'x00',
+    "right": b'x00',
+    "play": b'x00',
+    "pause": b'x00',
+    "stop": b'x00',
+    "fastforward": b'x00',
+    "rewind": b'x00',
+    "eject": b'x00',
+}
+
+try:
+    with open("/home/justin/.broadlinkBlurayControllerPackets", "wb") as file:
+        for name in packets:
+            count = 0
+            while True:
+                if count > 5:
+                    print("Quitting...")
+                    exit(1)
+                try:
+                    device.enter_learning()
+                    thing = input(f"Send {name} button, then press any key.")
+                    packets[name] = device.check_data()
+                    print(f"\nPacket determined: {packets[name]}\n")
+                    break
+                except: 
+                    print("Let's try that again...")
+                    count += 1
+        pickle.dump(packets, file)
+        print(f"Success. Here's the data that was saved: {packets}")
+
+except:
+    print("Failed to open the /home/justin/.broadlinkBlurayControllerPackets file!")
+
